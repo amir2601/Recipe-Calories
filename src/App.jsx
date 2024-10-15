41183d3825451c87c14afd70aefd875076060e38
amir2601 <amir26hossin@gmail.com>
2024-10-15 13:33:07 +0600

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className='container border mx-auto'>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+// import PropTypes from 'prop-types';
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-between items-center py-5">
+            <p className="text-2xl font-bold">Recipe Calories</p>
+
+            <ul className="flex gap-5 cursor-pointer">
+                <li>Home</li>
+                <li>Recipes</li>
+                <li>About</li>
+                <li>Search</li>
+            </ul>
+
+            <div className="flex items-center gap-4">
+                <div>
+                    <input className="border rounded-full p-3" type="search" name="" id="" placeholder="Search"/>
+                </div>
+
+                <div className="avatar">
+                    <div className="w-12 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Navbar.propTypes = {
+
+};
+
+export default Navbar;
